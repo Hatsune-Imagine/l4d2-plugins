750c01cf44c1b726d93c74f58c9b9ea519527099
Hatsune Imagine <2219385800@qq.com>
2024-03-08 17:33:36 +0800

    Add scripts for 'l4d2_map_vote'

---
+++ l4d2_map_vote/map_order_reset.bat
@@ -0,0 +1,9 @@
+del left4dead2\addonlist.txt
+del left4dead2\addons\sourcemod\translations\chapters.phrases.txt
+del left4dead2\addons\sourcemod\translations\missions.phrases.txt
+del left4dead2\addons\sourcemod\translations\chi\chapters.phrases.txt
+del left4dead2\addons\sourcemod\translations\chi\missions.phrases.txt
+copy left4dead2\addons\sourcemod\translations\chapters.phrases.bak left4dead2\addons\sourcemod\translations\chapters.phrases.txt
+copy left4dead2\addons\sourcemod\translations\missions.phrases.bak left4dead2\addons\sourcemod\translations\missions.phrases.txt
+copy left4dead2\addons\sourcemod\translations\chi\chapters.phrases.bak left4dead2\addons\sourcemod\translations\chi\chapters.phrases.txt
+copy left4dead2\addons\sourcemod\translations\chi\missions.phrases.bak left4dead2\addons\sourcemod\translations\chi\missions.phrases.txt
---
+++ l4d2_map_vote/map_order_reset.sh
@@ -0,0 +1,9 @@
+rm left4dead2/addonlist.txt
+rm left4dead2/addons/sourcemod/translations/chapters.phrases.txt
+rm left4dead2/addons/sourcemod/translations/missions.phrases.txt
+rm left4dead2/addons/sourcemod/translations/chi/chapters.phrases.txt
+rm left4dead2/addons/sourcemod/translations/chi/missions.phrases.txt
+cp left4dead2/addons/sourcemod/translations/chapters.phrases.bak left4dead2/addons/sourcemod/translations/chapters.phrases.txt
+cp left4dead2/addons/sourcemod/translations/missions.phrases.bak left4dead2/addons/sourcemod/translations/missions.phrases.txt
+cp left4dead2/addons/sourcemod/translations/chi/chapters.phrases.bak left4dead2/addons/sourcemod/translations/chi/chapters.phrases.txt
+cp left4dead2/addons/sourcemod/translations/chi/missions.phrases.bak left4dead2/addons/sourcemod/translations/chi/missions.phrases.txt
